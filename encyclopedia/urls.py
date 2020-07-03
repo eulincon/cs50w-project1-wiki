1df6e292e5efc59d00680258cdbdfe7805f4932e
Lincon Brito <38511541+zlincon@users.noreply.github.com>
2020-07-03 15:49:40 -0300

    #

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('a', views.a, name="a"),
     path('newpage', views.newpage, name='newpage'),
     path('upload', views.upload, name='upload'),
-    path('editpage/<str:entry>', views.editpage, name='editpage'),
     path('savechanges', views.editpage, name='savechanges'),
-    path('<str:entry>', views.entry, name='entry'),
+    path('randompage', views.randompage, name='randompage'),
+    path('editpage/<str:entry>', views.editpage, name='editpage'),
+    path('wiki/<str:entry>', views.entry, name='entry')
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -3,6 +3,8 @@ from django.http import HttpRequest, HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.core.files.storage import default_storage
 from django.core.files import File
+from random import choice
+import markdown2
 
 from . import util
 
@@ -14,34 +16,44 @@ def index(request):
 		        "entries": util.list_entries()
 		    })
 		elif q is not None:
-			return HttpResponseRedirect(f'{q}')
+			content = util.get_entry(q)
+			if content:
+				return HttpResponseRedirect(f'wiki/{q}')
+			elif content is None:
+				entries = util.list_entries()
+				entries = [i for i in entries if i.lower().find(q.lower()) != -1]
+				return render(request, 'encyclopedia/index.html',{
+					'entries': entries
+				})
 
 def newpage(request):
 	return render(request, 'encyclopedia/newpage.html',{})
 
 def entry(request, entry):
 	content = util.get_entry(entry)
+	#return HttpResponse(request.handler404)
 	if content:
+		html = markdown2.markdown(content)
 		return render(request, 'encyclopedia/entry.html', {
 			'title': entry,
-			'entry': content
+			'entry': html
 		})
 	elif content is None:
-		entries = util.list_entries()
-		entries = [i for i in entries if i.lower().find(entry.lower()) != -1]
-		return render(request, 'encyclopedia/index.html', {
-			'entries': entries
+		return render(request, 'encyclopedia/error.html', {
+			'error': 404
 		})
 
+	
 def upload(request):
 	title = request.POST.get('title')
 	content = request.POST.get('content')
 	if default_storage.exists(f'entries/{title}.md'):
-		return HttpResponse('Error: Directory already content')
-		#Undone
+		return render(request, 'encyclopedia/error.html', {
+			'error': 2
+		})
 	else:
 		util.save_entry(title, content)
-		return HttpResponseRedirect(f'{title}')
+		return HttpResponseRedirect(f'wiki/{title}')
 
 def editpage(request, entry='git'):
 	title = entry
@@ -55,6 +67,15 @@ def editpage(request, entry='git'):
 		title = request.POST.get('title')
 		content = request.POST.get('content')
 		util.save_entry(title, content)
-		return HttpResponseRedirect(f'{title}')
+		return HttpResponseRedirect(f'wiki/{title}')
+
+def randompage(request):
+	entries = util.list_entries()
+	return HttpResponseRedirect(f'wiki/{choice(entries)}')
+
+def a(request):
+	markdown = util.get_entry('html')
+	html = markdown2.markdown(markdown)
+	return HttpResponse(html)
 
 
