d8e78a35c63ba8eeccd225f7ec235939b53a4721
zlincon <38511541+zlincon@users.noreply.github.com>
2020-07-03 08:35:03 -0300

    #

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path('newpage', views.newpage, name='newpage'),
     path('upload', views.upload, name='upload'),
+    path('editpage/<str:entry>', views.editpage, name='editpage'),
+    path('savechanges', views.editpage, name='savechanges'),
     path('<str:entry>', views.entry, name='entry'),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponseRedirect, HttpResponse
 from django.urls import reverse
 from django.core.files.storage import default_storage
+from django.core.files import File
 
 from . import util
 
@@ -19,12 +20,13 @@ def newpage(request):
 	return render(request, 'encyclopedia/newpage.html',{})
 
 def entry(request, entry):
-	exist = util.get_entry(entry)
-	if exist:
+	content = util.get_entry(entry)
+	if content:
 		return render(request, 'encyclopedia/entry.html', {
-			'entry': exist
+			'title': entry,
+			'entry': content
 		})
-	elif exist is None:
+	elif content is None:
 		entries = util.list_entries()
 		entries = [i for i in entries if i.lower().find(entry.lower()) != -1]
 		return render(request, 'encyclopedia/index.html', {
@@ -32,7 +34,27 @@ def entry(request, entry):
 		})
 
 def upload(request):
-	
-	return HttpResponse(default_storage.exists('entries/Git.md'))
+	title = request.POST.get('title')
+	content = request.POST.get('content')
+	if default_storage.exists(f'entries/{title}.md'):
+		return HttpResponse('Error: Directory already content')
+		#Undone
+	else:
+		util.save_entry(title, content)
+		return HttpResponseRedirect(f'{title}')
+
+def editpage(request, entry='git'):
+	title = entry
+	content = util.get_entry(entry)
+	if request.method == 'GET':
+		return render(request, 'encyclopedia/editpage.html',{
+			'title': title,
+			'content': content
+		})
+	elif request.method == 'POST':
+		title = request.POST.get('title')
+		content = request.POST.get('content')
+		util.save_entry(title, content)
+		return HttpResponseRedirect(f'{title}')
 
 
